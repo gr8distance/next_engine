f79a13de7d3f8608b51a68f4a0d143195d6e5ee7
ug <gr8.distance@gmail.com>
2021-04-14 18:21:32 +0900

    🍷 VCR

--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule NextEngine.MixProject do
   defp deps do
     [
       {:httpoison, "~> 1.8"},
-      {:jason, "~> 1.2"}
+      {:jason, "~> 1.2"},
+      {:exvcr, "~> 0.11", only: :test}
     ]
   end
 end
